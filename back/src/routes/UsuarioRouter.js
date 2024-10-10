d1913a73a520b70be61b816f8992faf8566ccf5c
MURILO MACIEL DALL OLMO <04776294079@senacrs.edu.br>
2024-10-10 12:05:26 -0300

    Swagger atualizado

--- back/src/routes/UsuarioRouter.js
+++ back/src/routes/UsuarioRouter.js
@@ -4,7 +4,59 @@ const { storeUsuario, loginUsuario, getUser } = require('../controller/UsuarioCo
 const router = Router();
 
 router.post('/store/usuario', storeUsuario);
+/**
+* @swagger
+* /store/usuario:
+*   get:
+*     summary: Armazenamento dos usuarios
+*     responses:
+*       200:
+*         description: Lista dos usuários cadastrados no site
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*/
+
 router.post('/login', loginUsuario);
-router.get('/getUser/:id', getUser)
+/**
+* @swagger
+* /login:
+*   get:
+*     summary: Usuários já cadastrados para Login
+*     responses:
+*       200:
+*         description: Armazenamento para todos usuários cadastrados, autorizando Login
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*/
+
+
+router.get('/getUser/:id', getUser);
+/**
+* @swagger
+* /getUser/:id:
+*   get:
+*     summary: Seleciona o usuário
+*     responses:
+*       200:
+*         description: Seletor de usuário, movendo-o para a aba de perfil
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*/
+
+
+
+
 
 module.exports = router;
--- back/src/routes/imagemRouter.js
+++ back/src/routes/imagemRouter.js
@@ -6,6 +6,21 @@ const router = Router();
 const { storeImagem } = require('../controller/imagemController');
 
 // rota pra armazenar imagens
+/**
+* @swagger
+* /store/imagem:
+*   get:
+*     summary: Retorna todas as tarefas
+*     responses:
+*       200:
+*         description: Uma lista de tarefas
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 type: object
+*/
 router.post('/store/imagem', storeImagem);
 
 // exporta pra usar em outros arquivos
